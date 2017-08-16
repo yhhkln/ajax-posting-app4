a54f8de78364d8dca38a9ff0d86f46839cf41468
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-16 18:51:42 +0800

    plugin cont

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,6 +64,19 @@ class PostsController < ApplicationController
 
 	end
 
+	def rate
+		@post = Post.find(params[:id])
+
+		existing_score = @post.find_score(current_user)
+		if existing_score
+			existing_score.update( :score => params[:score])
+		else
+			@post.scores.create( :score => params[:score], :user => current_user )
+		end
+		render :json => { :average_score => @post.average_score }
+
+	end
+
 
 	protected
 
---
+++ app/models/post_score.rb
@@ -0,0 +1,5 @@
+class PostScore < ApplicationRecord
+
+	belongs_to :post
+	belongs_to :user
+end
